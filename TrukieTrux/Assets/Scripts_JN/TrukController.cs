5599ec6ac9866e5d86e3accd84e6973cff9d6d85
Tmaclea6 <tmaclea6@unb.ca>
2023-11-14 09:06:43 -0400

    Improved Truck Controller

--- TrukieTrux/Assets/Scripts_JN/TrukController.cs
+++ TrukieTrux/Assets/Scripts_JN/TrukController.cs
@@ -6,7 +6,7 @@ public class TrukController : MonoBehaviour
 {
     private Vector3 velocity;
     public int gear = 1;
-    public float speed = 2f;
+    public float speed = 0;
     private Rigidbody rb;
     private float movementX;
     private float movementZ = 2.0f;
@@ -14,16 +14,33 @@ public class TrukController : MonoBehaviour
     private bool stall = false;
     
     public float maxSpeed = 10f;
+
+    public float direction = 90;
     
     public GearChangingUI gearChangeUI;
+    public float torque = 1;
 
 
+    public Vector3 RotateVectorAroundY(float angleDegrees, Vector3 originalVector)
+    {
+        // Convert angle to radians
+        float angleRadians = Mathf.Deg2Rad * angleDegrees;
+
+        // Create a quaternion for the rotation around the Y axis
+        Quaternion rotationQuaternion = Quaternion.Euler(0, angleDegrees, 0);
+
+        // Apply the rotation to the original vector
+        Vector3 rotatedVector = rotationQuaternion * originalVector;
+
+        return rotatedVector;
+    }
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         timeSinceStart = 0;
         velocity = new Vector3(0f, 0f, 0f);
+
     }
 
     // Update is called once per frame
@@ -32,51 +49,57 @@ public class TrukController : MonoBehaviour
         gear = gearChangeUI.GetGear();
         if (gear != 0 && Input.GetKey("w"))
         {
-            if(rb.velocity.z < maxSpeed *gear){
-                movementZ = speed * gear;
-                velocity = new Vector3(0f, 0f, movementZ); 
-                rb.AddRelativeForce(velocity * Time.deltaTime);
+            if(speed < maxSpeed *gear){
+                 if(speed <= maxSpeed*gear){
+                    speed += 2f * Time.deltaTime;
+                 }
+                rb.AddForce(transform.forward*speed);
             }
               
         }
         else{
-            if(rb.velocity.z > 0 ){
-                movementZ = speed * (1/1+gear);
-                velocity = new Vector3(0f, 0f, -movementZ); 
-                rb.AddForce(velocity * Time.deltaTime);
+            if(speed > 0 ){
+                speed -= 2f;
+                rb.AddForce(transform.forward*speed);
             }
         }
         if(Input.GetKey(KeyCode.Space)){
             if(Input.GetKeyDown("s")){
-                velocity = new Vector3(0f, 0f, -10f);
-                rb.AddForce(velocity);
+                speed -= 2f;
+                rb.AddForce(transform.forward*speed);
             }
-              if(rb.velocity.z > 0 ){
-                movementZ = speed * (1/1+gear);
+              if(speed > 0 ){
+                speed -= (1/(1+gear));
                 velocity = new Vector3(0f, 0f, -movementZ); 
-                rb.AddForce(velocity * Time.deltaTime);
+                rb.AddForce(transform.forward*speed);
             } 
         }
         if (Input.GetKey("a"))
         {
-            if(rb.velocity.x > -speed){
-                velocity = new Vector3(-speed, 0f, 0f);  
-                rb.AddForce(velocity);
-             }
+            // if(rb.velocity.x > -speed){
+            //     velocity = new Vector3(-speed, 0f, 0f);  
+            //     rb.AddForce(velocity);
+            //  }
+           
+            //transform.Rotate(0, 1, 0, Space.Self);
+            float turn = -5f;
+            rb.AddTorque(transform.up * Time.deltaTime * turn);
         }
-        else{
-            rb.velocity = new Vector3(0f, rb.velocity.y, rb.velocity.z);
-        }
+       
         if (Input.GetKey("d"))
         {
-            if(rb.velocity.x < speed){
-                velocity = new Vector3(speed, 0f, 0f);  
-                rb.AddForce(velocity);
-             } 
+            // if(rb.velocity.x < speed){
+            //     velocity = new Vector3(speed, 0f, 0f);  
+            //     rb.AddForce(velocity);
+            //  } 
+            
+            //direction +=1;
+            float turn = 5f;
+            rb.AddTorque(transform.up * Time.deltaTime * turn);
+            
+            //transform.Rotate(0, -1, 0, Space.Self);
         }
-        else{
-            rb.velocity = new Vector3(0f, rb.velocity.y, rb.velocity.z);
-        }
+       
         if (Input.GetKey("s"))
         {
             stall = true;     
