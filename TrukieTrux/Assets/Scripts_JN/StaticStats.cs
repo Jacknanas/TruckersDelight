44984006af34e14a59aceaec54e93f9f4b978805
Tmaclea6 <tmaclea6@unb.ca>
2023-11-27 10:29:17 -0400

    Assets and staticClass

---
+++ TrukieTrux/Assets/Scripts_JN/StaticStats.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StaticStats
+{
+    public static Run run { get; set; }
+    public static TruckStats truckStats { get; set; }
+}
